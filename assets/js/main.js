0e97c226f4467ebd210b4bc3e1255c23a264a824
Amantay <amantay.sv@gmail.com>
2021-01-28 23:42:22 +0600

    123

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,20 @@ VANTA.WAVES({
   waveSpeed: 0.50,
   zoom: 0.65
 });
+
+function SmoothVerticalScrolling(e, time, where) {
+  var eTop = e.getBoundingClientRect().top;
+  var eAmt = eTop / 100;
+  var curTime = 0;
+  while (curTime <= time) {
+      window.setTimeout(SVS_B, curTime, eAmt, where);
+      curTime += time / 100;
+  }
+}
+
+function SVS_B(eAmt, where) {
+  if(where == "center" || where == "")
+      window.scrollBy(0, eAmt / 2);
+  if (where == "top")
+      window.scrollBy(0, eAmt);
+}
