a1c72221d2a3eaae5105a3c0bd4fb6531e84e962
Amantay <amantay.sv@gmail.com>
2021-01-28 23:52:16 +0600

    123

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,10 @@ function SVS_B(eAmt, where) {
   if (where == "top")
       window.scrollBy(0, eAmt);
 }
+
+import smoothscroll from 'smoothscroll-polyfill';
+// or if linting/typescript complains
+import * as smoothscroll from 'smoothscroll-polyfill';
+
+// kick off the polyfill!
+smoothscroll.polyfill();
