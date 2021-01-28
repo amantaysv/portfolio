accde4d35e54127f300d3b5185465b14411418b0
Amantay <amantay.sv@gmail.com>
2021-01-28 16:05:35 +0600

    123

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 VANTA.WAVES({
-    el: ".promo",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x1934
-  })
+  el: ".promo",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x435f,
+  shininess: 90.00,
+  waveHeight: 40.00,
+  waveSpeed: 0.50,
+  zoom: 0.65
+})
