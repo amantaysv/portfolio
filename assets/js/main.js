d39e2b755ec334ecf5b6421324a272aa625b228f
Amantay <amantay.sv@gmail.com>
2021-01-29 00:00:02 +0600

    brand

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,27 +17,3 @@ VANTA.WAVES({
   waveSpeed: 0.50,
   zoom: 0.65
 });
-
-function SmoothVerticalScrolling(e, time, where) {
-  var eTop = e.getBoundingClientRect().top;
-  var eAmt = eTop / 100;
-  var curTime = 0;
-  while (curTime <= time) {
-      window.setTimeout(SVS_B, curTime, eAmt, where);
-      curTime += time / 100;
-  }
-}
-
-function SVS_B(eAmt, where) {
-  if(where == "center" || where == "")
-      window.scrollBy(0, eAmt / 2);
-  if (where == "top")
-      window.scrollBy(0, eAmt);
-}
-
-import smoothscroll from 'smoothscroll-polyfill';
-// or if linting/typescript complains
-import * as smoothscroll from 'smoothscroll-polyfill';
-
-// kick off the polyfill!
-smoothscroll.polyfill();
